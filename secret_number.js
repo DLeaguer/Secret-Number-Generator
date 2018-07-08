1ae23dd8005d664d2f150749081aef117072de7b
DLeaguer <immostlikely@hotmail.com>
2018-07-07 15:56:57 -1000

    complete

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,25 @@
 'use strict';
 module.exports = function() {
-  
+  const rand = Math.floor(Math.random()*1000000);
+  console.log('rand', rand);
+  return function() {
+    return rand;
+  }
 };
+
+
+// randomNum() // different num
+
+// const myNum = randomNum()
+// myNum() //same num
+// myNum()
+// myNum()
+// myNum()
+
+// const myNum2 = randomNum()
+// myNum2()
+// myNum2()
+// myNum2()
+// myNum2()
+
+// myNum()
