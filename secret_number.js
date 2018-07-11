b505666e1322776582428e3177883c801753d3c4
DLeaguer <immostlikely@hotmail.com>
2018-07-10 19:09:52 -1000

    added comments & call functions without console.logs

--- secret_number.js
+++ secret_number.js
@@ -8,10 +8,58 @@ module.exports = function randomNum() {
   }
 };
 
-// const myNum = randomNum() sets all numbers to a new number
-// randomNum() is the outer rand & shows a different number than the set number
-// myNum() is the inner rand & shows the same number as the set number
+// WHAT EACH FUNCTION CALL DOES
 
+// const myNum = randomNum() sets all numbers to a new number
+// randomNum() is rand outer & shows a different number than the set number
+// myNum() is rand inner & shows the same number as the set number
+
+
+//========================================================================================================== 
+
+
+// CALL FUNCTIONS WITHOUT CONSOLE.LOGs TO SEE CLEARER WHAT IS CALLED
+
+      // call function and set it to myNum
+
+// const myNum = randomNum();     // number set to myNum
+
+// myNum();                       // myNum stays constantly the same 
+
+// randomNum();                   // rand outer changed but rand inner still the same
+
+// myNum();                       // rand inner still the same 
+
+      // call function and set it to myNum2
+
+// const myNum2 = randomNum();    // all numbers reset to a new number
+
+// myNum2();                      // constant
+
+// randomNum();                   // rand outer random but rand inner same
+
+// myNum2();                      // rand inner same      
+
+// myNum();     // wanted to see if it kept the same number from first function calling set  
+                // it did, see console.log results below
+
+
+//===========================================================================================================
+
+
+// CONSOLE.LOG TO SEE WHAT IT LOOKS LIKE
+
+// console.log('  randomNum  ', randomNum);
+// console.log('  myNum  ', myNum);
+// console.log('  myNum2  ', myNum2);
+
+
+//==========================================================================================================
+
+
+// CALL FUNCTIONS WITH CONSOLE.LOGs
+
+// call function and set it to myNum
 // console.log('  randomNum 1  ');
 // const myNum = randomNum();
 
@@ -39,6 +87,11 @@ module.exports = function randomNum() {
 // console.log('  myNum  ', myNum);
 // console.log('  myNum2  ', myNum2);
 
+
+//===========================================================================================================
+
+
+// EXAMPLE OF THE CONSOLE.LOGs results 
 
 // "  randomNum 1  "
 // "  rand  outer 421019"
